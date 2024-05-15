486228dafb6dd291336ede637381b26c007f1e09
Callum Smale <callum.smale@morrisonsplc.co.uk>
2024-05-15 16:21:56 +0100

    third commit

---
+++ src/app/ssr/[name]/page.tsx
@@ -0,0 +1,39 @@
+import { getDateString, getRandomUUID } from "../../utils";
+
+export const dynamic = "force-dynamic";
+
+async function getPokemon(name: string) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  if (!res.ok) throw new Error();
+  return res.json();
+}
+
+export default async function Page({ params }: { params: { name: string } }) {
+  const pokemon = await getPokemon(params.name);
+  return (
+    <main className="content">
+      <h1 className="heading">A server generated page!</h1>
+
+      <section className="data-container">
+        <article className="card">
+          <p>Generated</p>
+          <h2>{getDateString()}</h2>
+        </article>
+        <article className="card">
+          <p>UUID</p>
+          <h2>{getRandomUUID()}</h2>
+        </article>
+      </section>
+      <section className="data-container">
+        <article className="card">
+          <p>Generated</p>
+          <h2>{pokemon.name}</h2>
+        </article>
+        <article className="card">
+          <p>UUID</p>
+          <h2>{pokemon.weight}</h2>
+        </article>
+      </section>
+    </main>
+  );
+}
