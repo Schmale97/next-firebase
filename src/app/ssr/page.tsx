0faf8b2b165227a88793947ff6ba9421d9c6251a
Callum Smale <callum.smale@morrisonsplc.co.uk>
2024-05-15 16:17:08 +0100

    second commit

--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -2,7 +2,14 @@ import { getDateString, getRandomUUID } from "../utils";
 
 export const dynamic = "force-dynamic";
 
-export default function Page() {
+async function getPokemon(name: string) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  if (!res.ok) throw new Error();
+  return res.json();
+}
+
+export default async function Page() {
+  const pokemon = await getPokemon("ditto");
   return (
     <main className="content">
       <h1 className="heading">A server generated page!</h1>
@@ -15,6 +22,16 @@ export default function Page() {
         <article className="card">
           <p>UUID</p>
           <h2>{getRandomUUID()}</h2>
+        </article>
+      </section>
+      <section className="data-container">
+        <article className="card">
+          <p>Generated</p>
+          <h2>{pokemon.name}</h2>
+        </article>
+        <article className="card">
+          <p>UUID</p>
+          <h2>{pokemon.weight}</h2>
         </article>
       </section>
     </main>
